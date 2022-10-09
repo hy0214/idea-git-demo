ead589a931993b5320efa2e51380959021f235f4
hy0214 <115328374+hy0214@users.noreply.github.com>
2022-10-09 14:17:43 +0800

    http2

--- src/main/java/com/git/GitTest.java
+++ src/main/java/com/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello gitHub hot-fix two!!!");
         System.out.println("hello gitHub idea-git-demo");
         System.out.println("hello gitHub http");
+        System.out.println("hello gitHub http2");
     }
 }
